a96b1f77effc8dcd8916b8e42edcfadfbac3d142
James Wu <ichangjwu@outlook.com>
2022-11-21 07:58:01 -0800

    Add MUI

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import CssBaseline from '@mui/material/CssBaseline';
 import App from './App';
 
 const root = ReactDOM.createRoot(
@@ -7,6 +8,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>
 );
