cf7d8c5478cb2f178b520b4b0817bc045f1c6c9f
James Wu <ichangjwu@outlook.com>
2022-11-23 05:06:26 +0900

    Refactor styling of dropdownList

--- src/components/Header/DropdownList/DropdownList.tsx
+++ src/components/Header/DropdownList/DropdownList.tsx
@@ -5,24 +5,16 @@ import pacman from "../../../assets/images/pacman.png";
 import mike from "../../../assets/images/mike.png";
 import "./dropdownList.css";
 import CharacterListItem from "./CharacterListItem";
+import { StyledEngineProvider } from '@mui/material/styles';
 
 export default function DropdownList() {
   return (
-    <>
-      <List
-        sx={{
-          position: "absolute",
-          right: 0,
-          top: "45px",
-          backgroundColor: "#696969",
-          opacity: "0.9",
-          width: "280px",
-        }}
-      >
+    <StyledEngineProvider injectFirst>
+      <List className="dropdown-character-list">
         <CharacterListItem imageSrc={farnsworth} name="Farnsworth" />
         <CharacterListItem imageSrc={pacman} name="Pac-Man" />
         <CharacterListItem imageSrc={mike} name="Mike" />
       </List>
-    </>
+    </StyledEngineProvider>
   );
 }
