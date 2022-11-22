0a1d108ee4f5b13280735203c7f22908d957e85d
James Wu <ichangjwu@outlook.com>
2022-11-23 01:40:27 +0900

    Import global css

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import CssBaseline from '@mui/material/CssBaseline';
 import App from './App';
+import './global.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
