0ad92c9e2b0683056e7c695bd218899e7712b78d
James Wu <ichangjwu@outlook.com>
2022-11-23 02:27:29 +0900

    Create MainImage component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-
 import Header from './components/Header/Header';
+import MainImage from './components/Main/MainImage';
 
 function App() {
   return (
     <div>
       <Header />
+      <MainImage />
     </div>
   );
 }
