e8d148ec8ba134ef37d427599f903aba4d2f6c35
James Wu <ichangjwu@outlook.com>
2022-11-23 02:54:23 +0900

    Add game image

--- src/components/Header/Counter/Counter.tsx
+++ src/components/Header/Counter/Counter.tsx
@@ -5,7 +5,7 @@ export default function Counter() {
   return (
     <div className="counter-container">
       <span className="counter-number">3</span>
-      <DropdownList />
+      
     </div>
   );
 }
