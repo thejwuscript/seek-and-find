87a335dd472ccf96dc4e2554716fc736aca44c8e
James Wu <ichangjwu@outlook.com>
2022-11-24 20:43:29 +0900

    Render Timer conditionally

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
-import Counter from './Counter/Counter';
-import HomeIcon from '@mui/icons-material/Home';
-import Timer from './Timer/Timer';
-import './header.css';
+import React from "react";
+import Counter from "./Counter/Counter";
+import HomeIcon from "@mui/icons-material/Home";
+import Timer from "./Timer/Timer";
+import "./header.css";
+import "./timer.css";
 
-export default function Header() {
+type Props = {
+  gameStart: boolean;
+};
+
+export default function Header({ gameStart }: Props) {
+  let timer = gameStart ? <Timer /> : <div className="timer">00:00</div>;
   return (
     <header>
-      <div className='header-content'>
-        <HomeIcon sx={{color: "white", fontSize: "28px"}} />
-        <Timer />
+      <div className="header-content">
+        <HomeIcon sx={{ color: "white", fontSize: "28px" }} />
+        {timer}
         <Counter />
       </div>
     </header>
-  )
-};
+  );
+}
--- src/components/Header/Timer/Timer.tsx
+++ src/components/Header/Timer/Timer.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import './timer.css';
 
-export default function Timer() {
+export default function Timer() { 
   return (
     <div className='timer'>01:24</div>
   )
