a8ff8346ae3880a50289bd132e0245300f864dcb
James Wu <ichangjwu@outlook.com>
2022-11-24 22:21:49 +0900

    Implement timer function and styling

--- src/components/Header/Timer/Timer.tsx
+++ src/components/Header/Timer/Timer.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo } from "react";
 
-export default function Timer() { 
+export default function Timer() {
+  const startTime = useMemo(() => Date.now(), []);
+  const [currentTime, setCurrentTime] = useState(startTime);
+
+  useEffect(() => {
+    const interval = setInterval(() => setCurrentTime(Date.now()), 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const diff = currentTime - startTime;
+  let seconds = `${Math.floor((diff / 1000) % 60)}`.padStart(2, "0");
+  let minutes = `${Math.floor((diff / 1000 / 60) % 60)}`.padStart(2, "0");
+
   return (
-    <div className='timer'>01:24</div>
-  )
-};
+    <div className="timer">
+      <span>{minutes}</span>:<span>{seconds}</span>
+    </div>
+  );
+}
