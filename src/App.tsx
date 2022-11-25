549b5f346bb0536887822f66d85452cd90c84fa4
James Wu <ichangjwu@outlook.com>
2022-11-26 01:34:46 +0900

    Toggle menu visibility

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,21 @@ import HomeModal from "./components/Modal/HomeModal";
 function App() {
   const [gameStart, setGameStart] = useState(false);
   const [mainImageLoaded, setMainImageLoaded] = useState(false);
+  const [isContextMenuVisible, setIsContextMenuVisible] = useState(false);
 
   useEffect(() => {
     if (!gameStart) document.body.style.overflowY = "hidden";
     else document.body.style.overflowY = "scroll";
   }, [gameStart]);
 
+  const handleImageClick = (): void => {
+    setIsContextMenuVisible(!isContextMenuVisible);
+  }
+
   return (
     <div>
       <Header gameStart={gameStart} setGameStart={setGameStart} />
-      <MainImage setImageLoaded={setMainImageLoaded} />
+      <MainImage setImageLoaded={setMainImageLoaded} handleImageClick={handleImageClick} isMenuVisible={isContextMenuVisible} />
       {!gameStart && (
         <HomeModal setGameStart={setGameStart} gameReady={mainImageLoaded} />
       )}
--- src/components/Main/Image/MainImage.tsx
+++ src/components/Main/Image/MainImage.tsx
@@ -1,22 +1,24 @@
 import React from "react";
-import ContextMenu from '../ContextMenu/ContextMenu';
+import SelectMenu from '../SelectMenu/SelectMenu';
 import robotCity from "../../../assets/images/robot_city.jpg";
 import "./mainImage.css";
 
 type Props = {
   setImageLoaded: React.Dispatch<React.SetStateAction<boolean>>;
+  handleImageClick: React.MouseEventHandler<HTMLDivElement>;
+  isMenuVisible: boolean;
 };
 
-export default function MainImage({ setImageLoaded }: Props) {
+export default function MainImage({ setImageLoaded, handleImageClick, isMenuVisible }: Props) {
   return (
-    <div>
+    <div onClick={handleImageClick} >
       <img
         src={robotCity}
         alt="main"
         className="main-image"
         onLoad={() => setImageLoaded(true)}
       />
-      <ContextMenu />
+      {isMenuVisible && <SelectMenu />}
     </div>
   );
 }
