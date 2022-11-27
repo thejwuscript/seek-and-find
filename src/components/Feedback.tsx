33e9e652335832cfabe6c3b2ac9fb592f7e5977e
James Wu <ichangjwu@outlook.com>
2022-11-28 02:47:19 +0900

    Remove unused imports

--- src/components/Feedback.tsx
+++ src/components/Feedback.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import Snackbar from "@mui/material/Snackbar";
-import IconButton from "@mui/material/IconButton";
-import CloseIcon from "@mui/icons-material/Close";
 import Alert, { AlertColor } from "@mui/material/Alert";
 
 type Props = {
