a777d1fa0f7ad3c657fc142f3748ab22606d1865
James Wu <ichangjwu@outlook.com>
2022-11-29 02:49:45 +0900

    Build mockup of leaderboard hooked up to Firestore

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -1,9 +1,82 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import {
+  collection,
+  DocumentData,
+  getDocs,
+  orderBy,
+  query,
+} from "firebase/firestore";
+import { db } from "../firebase-config";
+import "./leaderboard.css";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Icon from "@mdi/react";
+import { mdiTrophyAward } from "@mdi/js";
 
 export default function Leaderboard() {
+  const [players, setPlayers] = useState<DocumentData[]>([]);
+
+  useEffect(() => {
+    const fetchPlayers = async () => {
+      let array: DocumentData[] = [];
+      const q = query(collection(db, "Players"), orderBy("time", "asc"));
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        let docObj = { ...doc.data(), id: doc.id };
+        array.push(docObj);
+      });
+      return array;
+    };
+
+    fetchPlayers().then((data) => setPlayers(data));
+  }, []);
+
   return (
-    <div>
-      <h1>Leaderboard Page</h1>
+    <div className="leaderboard-page">
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <Icon path={mdiTrophyAward} size={1.2} color="white" />
+        <h1 className="title">LEADERBOARD</h1>
+        <Icon path={mdiTrophyAward} size={1.2} color="white" />
+      </div>
+      <TableContainer
+        component={Paper}
+        sx={{
+          width: "95%",
+          maxWidth: 500,
+          border: "8px solid var(--black)",
+          borderRadius: "15px",
+          outline: "2px solid white",
+        }}
+      >
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell sx={{ fontWeight: "bold" }}>Name</TableCell>
+              <TableCell sx={{ fontWeight: "bold" }} align="right">
+                Time
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {players.map((player) => (
+              <TableRow
+                key={player.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {player.name}
+                </TableCell>
+                <TableCell align="right">{player.time}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
-  )
-};
+  );
+}
